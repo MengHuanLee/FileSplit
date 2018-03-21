8f799d0fc28604ff2e80d1860c3fbc755eafe3a0
Jerry_Lee <digital0117@gmail.com>
2018-03-21 16:25:12 -0700

    Add Crypt class

--- app/src/main/java/com/google/jerry/filesplit/MainActivity.java
+++ app/src/main/java/com/google/jerry/filesplit/MainActivity.java
@@ -49,6 +49,7 @@ public class MainActivity extends AppCompatActivity {
     private EditText mTargetPath;
     private static final int READ_REQUEST_CODE = 42;
     private static final int REQUEST_CODE_OPEN_DIRECTORY = 1;
+    private Crypt crypt = new Crypt();
 
     private String filename;
     public static long chunkSize = (long)(1.4 * 1024);
@@ -119,7 +120,7 @@ public class MainActivity extends AppCompatActivity {
         @Override
         protected Void doInBackground(String... filename) {
             try {
-                AESFileEncrypt(filename[0]);
+                crypt.AESFileEncrypt(filename[0]);
             } catch (Exception e){
                 Log.e("File", e.getMessage());
                 // Caused by: java.lang.RuntimeException: Can't create handler inside thread that has not called Looper.prepare()
@@ -138,7 +139,7 @@ public class MainActivity extends AppCompatActivity {
         @Override
         protected Void doInBackground(String... filename) {
             try {
-                AESFileDecryption(filename[0]);
+                crypt.AESFileDecryption(filename[0]);
             } catch (Exception e){
                 Log.e("File", e.getMessage());
                 // Caused by: java.lang.RuntimeException: Can't create handler inside thread that has not called Looper.prepare()
@@ -413,118 +414,7 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    public void AESFileEncrypt(String filename) throws Exception{
-        // file to be encrypted
-        FileInputStream inFile = new FileInputStream(filename);
 
-        // encrypted file
-        FileOutputStream outFile = new FileOutputStream(filename + ".encryptedfile.des");
-
-        // password to encrypt the file
-        String password = "javapapers";
-
-        // password, iv and salt should be transferred to the other end
-        // in a secure manner
-
-        // salt is used for encoding
-        // writing it to a file
-        // salt should be transferred to the recipient securely
-        // for decryption
-        byte[] salt = new byte[8];
-        SecureRandom secureRandom = new SecureRandom();
-        secureRandom.nextBytes(salt);
-        FileOutputStream saltOutFile = new FileOutputStream(filename + ".salt.enc");
-        saltOutFile.write(salt);
-        saltOutFile.close();
-
-        SecretKeyFactory factory = SecretKeyFactory
-                .getInstance("PBKDF2WithHmacSHA1");
-        KeySpec keySpec = new PBEKeySpec(password.toCharArray(), salt, 65536,
-                256);
-        SecretKey secretKey = factory.generateSecret(keySpec);
-        SecretKey secret = new SecretKeySpec(secretKey.getEncoded(), "AES");
-
-        //
-        Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
-        cipher.init(Cipher.ENCRYPT_MODE, secret);
-        AlgorithmParameters params = cipher.getParameters();
-
-        // iv adds randomness to the text and just makes the mechanism more
-        // secure
-        // used while initializing the cipher
-        // file to store the iv
-        FileOutputStream ivOutFile = new FileOutputStream(filename + "." + "iv.enc");
-        byte[] iv = params.getParameterSpec(IvParameterSpec.class).getIV();
-        ivOutFile.write(iv);
-        ivOutFile.close();
-
-        //file encryption
-        byte[] input = new byte[64];
-        int bytesRead;
-
-        while ((bytesRead = inFile.read(input)) != -1) {
-            byte[] output = cipher.update(input, 0, bytesRead);
-            if (output != null)
-                outFile.write(output);
-        }
-
-        byte[] output = cipher.doFinal();
-        if (output != null)
-            outFile.write(output);
-
-        inFile.close();
-        outFile.flush();
-        outFile.close();
-
-        System.out.println("File Encrypted.");
-    }
-
-    public void AESFileDecryption (String filename) throws Exception {
-
-            String password = "javapapers";
-
-            // reading the salt
-            // user should have secure mechanism to transfer the
-            // salt, iv and password to the recipient
-            FileInputStream saltFis = new FileInputStream(filename + ".salt.enc");
-            byte[] salt = new byte[8];
-            saltFis.read(salt);
-            saltFis.close();
-
-            // reading the iv
-            FileInputStream ivFis = new FileInputStream(filename + "." + "iv.enc");
-            byte[] iv = new byte[16];
-            ivFis.read(iv);
-            ivFis.close();
-
-            SecretKeyFactory factory = SecretKeyFactory
-                    .getInstance("PBKDF2WithHmacSHA1");
-            KeySpec keySpec = new PBEKeySpec(password.toCharArray(), salt, 65536,
-                    256);
-            SecretKey tmp = factory.generateSecret(keySpec);
-            SecretKey secret = new SecretKeySpec(tmp.getEncoded(), "AES");
-
-            // file decryption
-            Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
-            cipher.init(Cipher.DECRYPT_MODE, secret, new IvParameterSpec(iv));
-            FileInputStream fis = new FileInputStream(filename + ".encryptedfile.des");
-            FileOutputStream fos = new FileOutputStream(filename + ".decrypt");
-            byte[] in = new byte[64];
-            int read;
-            while ((read = fis.read(in)) != -1) {
-                byte[] output = cipher.update(in, 0, read);
-                if (output != null)
-                    fos.write(output);
-            }
-
-            byte[] output = cipher.doFinal();
-            if (output != null)
-                fos.write(output);
-            fis.close();
-            fos.flush();
-            fos.close();
-            System.out.println("File Decrypted.");
-        }
 
 
 }
