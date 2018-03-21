dd8f6ad0ebacce47fd02087fbb93b4bf6ddc2fe4
Jerry_Lee <digital0117@gmail.com>
2018-03-21 16:46:20 -0700

    Layout update

--- app/src/main/java/com/google/jerry/filesplit/MainActivity.java
+++ app/src/main/java/com/google/jerry/filesplit/MainActivity.java
@@ -83,7 +83,7 @@ public class MainActivity extends AppCompatActivity {
         mMerge.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                
+
             }
         });
 
