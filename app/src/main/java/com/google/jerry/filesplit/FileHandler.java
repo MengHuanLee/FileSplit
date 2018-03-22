ca0a46d8338dc8f56ec7835effb1d52ed9c75a1a
Jerry_Lee <digital0117@gmail.com>
2018-03-21 22:50:38 -0700

    Modify split file function
    Split to two files and return List<BufferedOutputStream>

--- app/src/main/java/com/google/jerry/filesplit/FileHandler.java
+++ app/src/main/java/com/google/jerry/filesplit/FileHandler.java
@@ -1,23 +1,27 @@
 package com.google.jerry.filesplit;
 
+import android.util.Log;
+
 import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Jerry on 3/21/18.
  */
 
 public class FileHandler {
-    public static long chunkSize = (long)(1.4 * 1024);
 
-    public static void split(String filename) throws IOException
+    public static List<BufferedOutputStream> split(String filename) throws IOException
     {
         // open the file
         BufferedInputStream in = new BufferedInputStream(new FileInputStream(filename));
+        List<BufferedOutputStream> listOfFile = new ArrayList<>();
 
         // get the file length
         File f = new File(filename);
@@ -25,38 +29,52 @@ public class FileHandler {
 
         // loop for each full chunk
         int subfile;
-        for (subfile = 0; subfile < fileSize / chunkSize; subfile++)
+        long chunkSize = fileSize/2;
+        for (subfile = 0; subfile < 2; subfile++)
         {
             // open the output file
             BufferedOutputStream out = new BufferedOutputStream(new FileOutputStream(filename + "." + subfile));
-
+            listOfFile.add(out);
             // write the right amount of bytes
-            for (int currentByte = 0; currentByte < chunkSize; currentByte++)
-            {
-                // load one byte from the input file and write it to the output file
-                out.write(in.read());
+            if(fileSize %2 == 0){
+                for (long currentByte = 0; currentByte < chunkSize; currentByte++)
+                {
+                    // load one byte from the input file and write it to the output file
+                    out.write(in.read());
+                }
+            } else {
+                for (long currentByte = 0; currentByte < chunkSize + 1; currentByte++)
+                {
+                    // load one byte from the input file and write it to the output file
+                    out.write(in.read());
+                }
+                chunkSize -= 1;
             }
 
             // close the file
-            out.close();
+//            out.close();
         }
-
-        // loop for the last chunk (which may be smaller than the chunk size)
-        if (fileSize != chunkSize * (subfile - 1))
-        {
-            // open the output file
-            BufferedOutputStream out = new BufferedOutputStream(new FileOutputStream(filename + "." + subfile));
-
-            // write the rest of the file
-            int b;
-            while ((b = in.read()) != -1)
-                out.write(b);
-
-            // close the file
-            out.close();
+        if(in.read() != -1){
+            Log.i("File Split", "File split not completed.");
         }
+//        // loop for the last chunk (which may be smaller than the chunk size)
+//        if (fileSize != chunkSize * (subfile - 1))
+//        {
+//            // open the output file
+//            BufferedOutputStream out = new BufferedOutputStream(new FileOutputStream(filename + "." + subfile));
+//            Log.i("File Split", "Entering");
+//            // write the rest of the file
+//            int b;
+//            while ((b = in.read()) != -1)
+//                out.write(b);
+//
+//            // close the file
+//            out.close();
+//        }
 
         // close the file
         in.close();
+        Log.i("File Split", "Returning File List");
+        return listOfFile;
     }
 }
